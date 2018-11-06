b566f29724368b9bf738d9336af36c4ba2452833
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2018-11-06 19:54:49 +0900

    getdebuginfo b64encoded

--- embed.py
+++ embed.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import sys
 import re
+import base64
+import zlib
 
-if __name__ == '__main__':
+def main():
     pattern = re.compile( r'^(.*)#EMBED (.*)#(.*)' )
     match = False
     for line in sys.stdin:
@@ -11,9 +13,17 @@ if __name__ == '__main__':
             match = True
             prefix = m.group( 1 )
             fp = open(m.group( 2 ), 'r')
-            buffer = fp.read()
+            buf = fp.read()
             fp.close()
+            compressed = zlib.compress(buf.encode('utf-8'))
+            b64enc = base64.standard_b64encode(compressed)
+            chunks, chunk_size = len(b64enc), 64+9
+            s = '\\\n    '.join(["'{}'".format(b64enc[i:i+chunk_size]) for i in range(0, chunks, chunk_size) ])
             suffix = m.group( 3 )
-            print '{0}{1}{2}'.format(prefix, buffer, suffix)
+            print '{0}{1}{2}'.format(prefix, s, suffix)
         else:
             print line,
+    
+if __name__ == '__main__':
+    main()
+
