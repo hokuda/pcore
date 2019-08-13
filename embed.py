cad9439ca98dcc84a010fd63b07df58361c7cf9c
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2019-08-13 23:18:49 +0900

    embed version

--- embed.py
+++ embed.py
@@ -3,14 +3,24 @@ import sys
 import re
 import base64
 import zlib
+import subprocess
+
+
+def get_version_info():
+    git_log_command = "git log --date local"
+    git_log_output = str(subprocess.check_output(git_log_command.split())).split('\n')
+    date_line = git_log_output[2].replace("Date:", "").strip()
+    comment_line = git_log_output[4].strip()
+    return date_line + " (" + comment_line + ")"
+
 
 def main():
-    pattern = re.compile( r'^(.*)\'#EMBED (.*)#(.*)\'' )
-    match = False
+    version_simple_pattern = "#VERSION#"
+    version_info = get_version_info()
+    getdebuginfo_regex_pattern = re.compile( r'^(.*)\'#EMBED (.*)#(.*)\'' )
     for line in sys.stdin:
-        m = pattern.match(line)
+        m = getdebuginfo_regex_pattern.match(line)
         if m:
-            match = True
             prefix = m.group( 1 )
             fp = open(m.group( 2 ), 'r')
             buf = fp.read()
@@ -22,6 +32,7 @@ def main():
             suffix = m.group( 3 )
             print '{0}{1}{2}'.format(prefix, s, suffix)
         else:
+            line = line.replace(version_simple_pattern, version_info, 1)
             print line,
     
 if __name__ == '__main__':
