7addbd471e707c40e78f54c1eec3ff92a772c37e
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2020-05-19 16:27:44 +0900

    fix

--- embed.py
+++ embed.py
@@ -25,7 +25,7 @@ def main():
         compressed = zlib.compress(buf.encode('utf-8'))
         b64enc = base64.standard_b64encode(compressed)
         chunks, chunk_size = len(b64enc), 64+9
-        folded_b64enc = '\\\n    '.join(["'{}'".format(b64enc[i:i+chunk_size]) for i in range(0, chunks, chunk_size) ])
+        folded_b64enc = '\\\n    '.join(["'{}'".format(b64enc[i:i+chunk_size].decode(encoding='utf-8')) for i in range(0, chunks, chunk_size) ])
     for line in sys.stdin:
         line = line.replace(version_simple_pattern, version_info, 1)
         line = line.replace(getdebuginfo_simple_pattern, folded_b64enc, 1)
