3d1ae8df60f03f347eae56a4cd07043287f32867
Hisanobu Okuda <hisanobu.okuda@gmail.com>
2020-05-19 18:12:00 +0900

    fix tests

--- test/podman/rhel8/run_test.sh
+++ test/podman/rhel8/run_test.sh
@@ -3,7 +3,6 @@
 mkdir tmp
 ../../../rhel8.sh < ../../../src/getdebuginfo > tmp/getdebuginfo
 ../../../rhel8.sh < ../../../src/pcore.main > tmp/pcore.main
-cp ../../run_get_debuginfo_unittest.py tmp/
 podman build -t ubi8/pcore_test .
 podman run ubi8/pcore_test /run_doctest.sh
 podman rmi -f ubi8/pcore_test
