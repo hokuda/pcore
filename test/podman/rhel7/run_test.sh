e93ea737667df9127332c951f21244a45ec9cb2a
hokuda <hisanobu.okuda@gmail.com>
2024-02-03 13:35:19 +0900

    remove unused things

--- test/podman/rhel7/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir tmp
-cp ../../../src/getdebuginfo tmp/
-cp ../../../src/pcore.main tmp/
-podman build -t rhel7/pcore_test .
-podman run rhel7/pcore_test /run_doctest.sh
-podman rmi -f rhel7/pcore_test
-rm -fr tmp
